4dbf7640433aa4b3381b987489c1b8dd53ff785b
Liz Shulman <corgipower@hotmail.com>
2020-09-12 14:12:05 -0400

    everything seems to work, need to do testing

---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,12 @@
+const { default: Axios } = require("axios");
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    return Axios.create({
+        baseURL: 'http://localhost:5000/api',
+        headers: {
+            Authorization: token,
+        }
+    })
+}
